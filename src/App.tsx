40b2db177c25575e0c483fadc0d24fcede3b6dd2
amirbagjni <amirbagjani@gmail.com>
2022-02-19 02:46:46 +0330

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,79 @@
-import React from 'react';
-import './App.css';
+import { Button, Checkbox, Container, Grid, TextField } from "@mui/material";
+import "./App.css";
+import {
+  toggleTodo,
+  updateTodo,
+  removeTodo,
+  addTodo,
+  TodosProvider,
+  useTodosContext,
+} from "./store";
 
-function App() {
+function ListItem() {
+  const {todos, toggleTodo, updateTodo, removeTodo} = useTodosContext();
   return (
-    <div className="App">
-     <h1>yoooo</h1>
-    </div>
+    <>
+      {todos.map((todo) => (
+        <Grid container gap={2} style={{ margin: `1rem` }} key={todo.id}>
+          <Checkbox onChange={() => toggleTodo(todo.id)} />
+          <TextField
+            value={todo.text}
+            onChange={(e) =>
+              updateTodo(e.target.value, todo.id)
+            }
+            variant="outlined"
+            size="small"
+          />
+          <Button
+            onClick={() => removeTodo(todo.id)}
+            variant="outlined"
+          >
+            Delete
+          </Button>
+        </Grid>
+      ))}
+    </>
   );
 }
+
+function AddItem() {
+  const {addTodo, setNewTodo, newTodo} = useTodosContext();
+
+  return (
+    <Grid container gap={9}>
+      <TextField
+        onChange={(e) => setNewTodo(e.target.value)}
+        value={newTodo}
+        variant="outlined"
+        size="small"
+      />
+      <Button onClick={addTodo} variant="outlined">
+        Add Todo
+      </Button>
+    </Grid>
+  );
+}
+
+function AppWrapper() {
+  const {todos} = useTodosContext();
+  console.log(todos);
+
+  return (
+    <>
+      <Container maxWidth="sm">
+        <ListItem />
+        <AddItem />
+      </Container>
+    </>
+  );
+}
+const App = () => (
+  <div className="App">
+    <TodosProvider>
+      <AppWrapper />
+    </TodosProvider>
+  </div>
+)
+
 
 export default App;
--- src/store.tsx
+++ src/store.tsx
@@ -1,3 +1,5 @@
+import { createContext, useState, useContext, useRef } from "react"
+
 export interface Todo{
     id: number;
     text: string;
@@ -16,4 +18,40 @@ export const toggleTodo = (todos: Todo[], id: number): Todo[] => (
 )
 export const updateTodo = (todos: Todo[], text: string, id: number): Todo[] => (
     todos.map(item => item.id === id ? {...item, text} : item)
+)
+
+//implement todos context
+export const useTodos = (initial: Todo[]) => {
+    const [todos,setTodos] = useState<Todo[]>(initial)
+    const [newTodo, setNewTodo] = useState("");
+
+    return {
+        todos,
+        newTodo,
+        setNewTodo,
+        addTodo(){
+            setTodos(addTodo(todos, newTodo))
+            setNewTodo("")
+        },
+        removeTodo(id: number){
+            setTodos(removeTodo(todos, id))
+        },
+        toggleTodo(id: number){
+            setTodos(toggleTodo(todos, id))
+        },
+        updateTodo(text: string, id: number){
+            setTodos(updateTodo(todos, text, id))
+        }
+    }
+}
+type UsetTodosType = ReturnType<typeof useTodos>
+
+
+const TodosContext = createContext<UsetTodosType>({} as UsetTodosType);
+export const useTodosContext = () =>  useContext(TodosContext);
+
+export const TodosProvider = ({children}: {children: React.ReactNode}) => (
+    <TodosContext.Provider value={useTodos([])}>
+        {children}
+    </TodosContext.Provider>
 )
