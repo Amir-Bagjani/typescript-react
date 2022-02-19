d8f4cccb0fb9c852e864b1a62bb427704d3d34df
amirbagjni <amirbagjani@gmail.com>
2022-02-19 14:35:36 +0330

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,80 @@
-import React from 'react';
-import './App.css';
+import { useState } from "react";
+import { Button, Checkbox, Container, Grid, TextField } from "@mui/material";
+import "./App.css";
+//redux
+import { Provider, useDispatch, useSelector } from "react-redux";
+import { addTodo, removeTodo, store, todosSelet, toggleTodo, updateTodo } from "./store";
 
-function App() {
+function ListItem() {
+  const todos = useSelector(todosSelet);
+  const dispatch = useDispatch()
+
   return (
-    <div className="App">
-     <h1>yoooo</h1>
-    </div>
+    <>
+      {todos.map((todo) => (
+        <Grid container gap={2} style={{ margin: `1rem` }} key={todo.id}>
+          <Checkbox
+            onChange={() => dispatch(toggleTodo(todo.id))}
+          />
+          <TextField
+            value={todo.text}
+            onChange={(e) => dispatch(updateTodo({text: e.target.value, id: todo.id})) }
+            variant="outlined"
+            size="small"
+          />
+          <Button
+            onClick={() => dispatch(removeTodo(todo.id))}
+            variant="outlined"
+          >
+            Delete
+          </Button>
+        </Grid>
+      ))}
+    </>
   );
 }
+
+function AddItem() {
+  const dispatch = useDispatch()
+  const [newTodo, setnewTodo] = useState("");
+
+  const handleAdd = () => {
+    dispatch(addTodo(newTodo));
+    setnewTodo("");
+  };
+
+  return (
+    <Grid container gap={9}>
+      <TextField
+        onChange={(e) => setnewTodo(e.target.value)}
+        value={newTodo}
+        variant="outlined"
+        size="small"
+      />
+      <Button onClick={handleAdd} variant="outlined">
+        Add Todo
+      </Button>
+    </Grid>
+  );
+}
+
+function AppWrapper() {
+  const todos = useSelector(todosSelet);
+  console.log(todos);
+
+  return (
+    <Container maxWidth="sm">
+      <ListItem />
+      <AddItem />
+    </Container>
+  );
+}
+const App = () => (
+  <div className="App">
+    <Provider store={store}>
+      <AppWrapper />
+    </Provider>
+  </div>
+);
 
 export default App;
--- src/store.tsx
+++ src/store.tsx
@@ -1,27 +1,61 @@
-import { useState } from "react"
+import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit" 
 
 export interface Todo{
     id: number;
     text: string;
     done: boolean;
 }
+interface InitialStateType {
+    todos: Todo[]
+}
 
-//helper finctions
-export const addTodo = (todos: Todo[], text: string): Todo[] => (
+//helper functions
+const addTodos = (todos: Todo[], text: string): Todo[] => (
     [...todos, {id: Math.random(), text, done: false }]
 )
-export const removeTodo = (todos: Todo[], id: number): Todo[] => (
+const removeTodos = (todos: Todo[], id: number): Todo[] => (
     todos.filter(item => item.id !== id)
 )
-export const toggleTodo = (todos: Todo[], id: number): Todo[] => (
+const toggleTodos = (todos: Todo[], id: number): Todo[] => (
     todos.map(item => item.id === id ? {...item, done: !item.done} : item)
 )
-export const updateTodo = (todos: Todo[], text: string, id: number): Todo[] => (
+const updateTodos = (todos: Todo[], text: string, id: number): Todo[] => (
     todos.map(item => item.id === id ? {...item, text} : item)
 )
 
 
-export const useTodos = (initial: Todo[]) => useState<Todo[]>(initial)
-export type UseTodosType = ReturnType<typeof useTodos>
-export type TodosType = UseTodosType[0]
-export type SetTodosType = UseTodosType[1]
+const initialState: InitialStateType = {
+    todos: []
+}
+
+//slice
+const todosSlice = createSlice({
+    name: "todos",
+    initialState,
+    reducers: {
+        addTodo : (state, action: PayloadAction<string> ) => {
+            state.todos = addTodos(state.todos, action.payload)
+        },
+        removeTodo: (state, action: PayloadAction<number>) => {
+            state.todos = removeTodos(state.todos, action.payload)
+        },
+        toggleTodo: (state, action: PayloadAction<number>) => {
+            state.todos = toggleTodos(state.todos, action.payload)
+        },
+        updateTodo: (state, action: PayloadAction<Omit<Todo, 'done'>>) => {
+            state.todos = updateTodos(state.todos, action.payload.text, action.payload.id)
+        }
+    }
+})
+export const { addTodo, removeTodo, toggleTodo, updateTodo } = todosSlice.actions
+
+
+//store
+export const store = configureStore({
+    reducer: {
+        todos: todosSlice.reducer
+    }
+})
+
+type RootState = ReturnType<typeof store.getState>
+export const todosSelet = (state: RootState) => state.todos.todos;
