5dbcc874ae90a9ec268b99638ceb821107111bba
amirbagjni <amirbagjani@gmail.com>
2022-02-19 04:45:19 +0330

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,71 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import { Button, Checkbox, Container, Grid, TextField } from "@mui/material";
+import "./App.css";
+import { useTodosReducer, DispatchTodosType, TodosType } from "./store";
+
+function ListItem({todos, dispatch}: {todos: TodosType, dispatch:DispatchTodosType}) {
+  return (
+    <>
+      {todos.map((todo) => (
+        <Grid container gap={2} style={{ margin: `1rem` }} key={todo.id}>
+          <Checkbox
+            onChange={() => dispatch({ type: "TOGGLE", payload: todo.id })}
+          />
+          <TextField
+            value={todo.text}
+            onChange={(e) =>
+              dispatch({
+                type: "UPDATE",
+                payload: { id: todo.id, text: e.target.value },
+              })
+            }
+            variant="outlined"
+            size="small"
+          />
+          <Button
+            onClick={() => dispatch({ type: "REMOVE", payload: todo.id })}
+            variant="outlined"
+          >
+            Delete
+          </Button>
+        </Grid>
+      ))}
+    </>
+  );
+}
+
+function AddItem({dispatch}: {dispatch:DispatchTodosType}) {
+  const [newTodo, setnewTodo] = useState("");
+  const handleAdd = () => {
+    dispatch({type: "ADD", payload: newTodo})
+    setnewTodo("");
+  };
+
+  return (
+    <Grid container gap={9}>
+      <TextField
+        onChange={(e) => setnewTodo(e.target.value)}
+        value={newTodo}
+        variant="outlined"
+        size="small"
+      />
+      <Button onClick={handleAdd} variant="outlined">
+        Add Todo
+      </Button>
+    </Grid>
+  );
+}
 
 function App() {
+  const [todos, dispatch] = useTodosReducer();
+  console.log(todos);
+
   return (
     <div className="App">
-     <h1>yoooo</h1>
+      <Container maxWidth="sm">
+        <ListItem todos={todos} dispatch={dispatch} />
+        <AddItem dispatch={dispatch} />
+      </Container>
     </div>
   );
 }
--- src/store.tsx
+++ src/store.tsx
@@ -1,19 +1,52 @@
-export interface Todo{
-    id: number;
-    text: string;
-    done: boolean;
+import { useReducer } from "react";
+
+export interface Todo {
+  id: number;
+  text: string;
+  done: boolean;
 }
 
-//helper finctions
-export const addTodo = (todos: Todo[], text: string): Todo[] => (
+type ActionType =
+  | { type: "ADD"; payload: string }
+  | { type: "REMOVE"; payload: number }
+  | { type: "TOGGLE"; payload: number }
+  | { type: "UPDATE"; payload: Omit<Todo, "done"> };
+
+//helper functions
+const addTodo = (todos: Todo[], text: string): Todo[] => (
     [...todos, {id: Math.random(), text, done: false }]
 )
-export const removeTodo = (todos: Todo[], id: number): Todo[] => (
+const removeTodo = (todos: Todo[], id: number): Todo[] => (
     todos.filter(item => item.id !== id)
 )
-export const toggleTodo = (todos: Todo[], id: number): Todo[] => (
-    todos.map(item => item.id === id ? {...item, done: !item.done} : item)
+const toggleTodo = (todos: Todo[], id: number): Todo[] => (
+    todos.map(item => item.id === id ? {...item, done: !item.done} : item)    
 )
-export const updateTodo = (todos: Todo[], text: string, id: number): Todo[] => (
+const updateTodo = (todos: Todo[], text: string, id: number): Todo[] => (
     todos.map(item => item.id === id ? {...item, text} : item)
 )
+
+const todoReducer = (state: Todo[], action: ActionType) => {
+  switch (action.type) {
+    case "ADD":
+      return addTodo(state, action.payload)
+
+    case "REMOVE":
+      return removeTodo(state, action.payload)
+
+    case "TOGGLE":
+      return toggleTodo(state, action.payload)
+
+    case "UPDATE":
+      return updateTodo(state, action.payload.text, action.payload.id)
+
+    default:
+      return state;
+  }
+};
+
+export const useTodosReducer = () => useReducer(todoReducer, []);
+
+export type UseTodosReducerType = ReturnType<typeof useTodosReducer>
+export type TodosType = UseTodosReducerType[0]
+export type DispatchTodosType = UseTodosReducerType[1]
